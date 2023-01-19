f74918d3018949c14b07533dc8b0bd65f57b9631
koppen1001 <88918564+koppen1001@users.noreply.github.com>
2023-01-19 14:49:42 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -2,10 +2,22 @@ function work(){
     document.getElementById('set_type').value='出勤'
     alert('出勤処理を実行しました。')
     submit()
-    
 }
+
 function worked(){
     document.getElementById('set_type').value='退勤'
     alert('退勤処理を実行しました。')
+    submit()
+}
+
+function ref(){
+    document.getElementById('set_type').value='休開'
+    alert('休憩開始処理を実行しました。')
+    submit()
+}
+
+function refed(){
+    document.getElementById('set_type').value='休終'
+    alert('休憩終了処理を実行しました。')
     submit()
 }
